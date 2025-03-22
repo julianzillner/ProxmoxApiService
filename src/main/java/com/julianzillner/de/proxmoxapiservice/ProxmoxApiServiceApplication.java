82cef5490cfc17a12b8319c0d466568d49fe7d9f
Julian Zillner <info@julianzillner.de>
2025-03-22 02:20:13 +0100

    update

---
+++ src/main/java/com/julianzillner/de/proxmoxapiservice/ProxmoxApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.julianzillner.de.proxmoxapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxmoxApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxmoxApiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/julianzillner/de/proxmoxapiservice/ProxmoxApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julianzillner.de.proxmoxapiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxmoxApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
